55b9edadf2d76a708d87badca155574fad9a5498
igor <12662101+alsaril@users.noreply.github.com>
2026-09-16 12:12:08 +0100

    tokenizer

--- scheme/src/main/kotlin/com/alsaril/scheme/tokenizer/tokens.kt
+++ scheme/src/main/kotlin/com/alsaril/scheme/tokenizer/tokens.kt
@@ -2,6 +2,14 @@ package com.alsaril.scheme.tokenizer
 
 sealed interface Token
 
-data class Number(val value: Int) : Token
+data class ConstantToken(val value: Int) : Token
 
-data class Symbol(val name: String) : Token
+data class SymbolToken(val name: String) : Token
+
+enum class BracketToken: Token {
+    OpenBracketToken, CloseBracketToken
+}
+
+data object DotToken: Token
+
+data object QuoteToken: Token
