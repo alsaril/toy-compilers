306baed044f6ae665610fd0ddc6709c016817468
igor <12662101+alsaril@users.noreply.github.com>
2026-09-16 12:12:08 +0100

    tokenizer

--- scheme/src/main/kotlin/com/alsaril/scheme/tokenizer/Tokenizer.kt
+++ scheme/src/main/kotlin/com/alsaril/scheme/tokenizer/Tokenizer.kt
@@ -1,6 +1,21 @@
 package com.alsaril.scheme.tokenizer
 
+import com.alsaril.scheme.tokenizer.BracketToken.CloseBracketToken
+import com.alsaril.scheme.tokenizer.BracketToken.OpenBracketToken
+
 object Tokenizer {
+    private fun isStartOfNumber(str: String, pos: Int): Boolean {
+        return str[pos].isDigit() || (pos < str.length - 1 && str[pos + 1].isDigit() && (str[pos] == '-' || str[pos] == '+'))
+    }
+
+    private fun parseSpecial(symbol: Char) = when (symbol) {
+        '(' -> OpenBracketToken
+        ')' -> CloseBracketToken
+        '.' -> DotToken
+        '\'' -> QuoteToken
+        else -> null
+    }
+
     fun tokenize(str: String): List<Token> {
         val result = mutableListOf<Token>()
 
@@ -12,14 +27,31 @@ object Tokenizer {
                 continue
             }
 
-            if (symbol.isDigit() || symbol == '-') {
+            if (isStartOfNumber(str, i)) {
                 val start = i++
                 while (i < str.length && str[i].isDigit()) i++
-                result.add(Number(str.substring(start, i).toInt()))
+                result.add(ConstantToken(str.substring(start, i).toInt()))
                 continue
             }
 
-            throw IllegalArgumentException("unknown symbol at $i: $symbol")
+            parseSpecial(symbol)?.let {
+                result.add(it)
+                i++
+                continue
+            }
+
+            if (symbol == '+') {
+                result.add(SymbolToken("+"))
+                i++
+                continue
+            }
+
+            run {// symbols
+                val start = i
+                while (i < str.length && str[i] != ' ' && str[i] != '+' && parseSpecial(str[i]) == null) i++
+                result.add(SymbolToken(str.substring(start, i)))
+                continue
+            }
         }
 
         return result
--- scheme/src/test/kotlin/com/alsaril/scheme/TokenizerTest.kt
+++ scheme/src/test/kotlin/com/alsaril/scheme/TokenizerTest.kt
@@ -1,24 +1,146 @@
 package com.alsaril.scheme
 
-import com.alsaril.scheme.tokenizer.Number
+import com.alsaril.scheme.tokenizer.*
+import com.alsaril.scheme.tokenizer.BracketToken.CloseBracketToken
+import com.alsaril.scheme.tokenizer.BracketToken.OpenBracketToken
 import com.alsaril.scheme.tokenizer.Tokenizer.tokenize
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class TokenizerTest {
     @ParameterizedTest
-    @CsvSource(
-        "'5', 5",
-        "'-5', -5",
-        "' 5001', 5001",
-        "' -90 ', -90"
-    )
-    fun `reads numbers`(input: String, expected: Int) {
+    @MethodSource("simple")
+    fun `tokenizes simple expression`(input: String, expected: List<Token>) {
         // when
         val output = tokenize(input)
 
         // then
-        assertThat(output).containsOnly(Number(expected))
+        assertThat(output).containsExactlyElementsOf(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("negative numbers")
+    fun `tokenizes negative numbers`(input: String, expected: List<Token>) {
+        // when
+        val output = tokenize(input)
+
+        // then
+        assertThat(output).containsExactlyElementsOf(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("spaces")
+    fun `correctly handles spaces`(input: String, expected: List<Token>) {
+        // when
+        val output = tokenize(input)
+
+        // then
+        assertThat(output).containsExactlyElementsOf(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("unary")
+    fun `correctly handles unary plus and minus`(input: String, expected: List<Token>) {
+        // when
+        val output = tokenize(input)
+
+        // then
+        assertThat(output).containsExactlyElementsOf(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("symbols")
+    fun `correctly handles symbol names`(input: String, expected: List<Token>) {
+        // when
+        val output = tokenize(input)
+
+        // then
+        assertThat(output).containsExactlyElementsOf(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("brackets")
+    fun `correctly handles brackets`(input: String, expected: List<Token>) {
+        // when
+        val output = tokenize(input)
+
+        // then
+        assertThat(output).containsExactlyElementsOf(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun simple() = listOf(
+            arguments("", listOf<Token>()),
+            arguments("2", listOf(ConstantToken(2))),
+            arguments("34", listOf(ConstantToken(34))),
+            arguments("  123", listOf(ConstantToken(123))),
+            arguments("hello", listOf(SymbolToken("hello"))),
+            arguments(" world", listOf(SymbolToken("world"))),
+            arguments(" 4+)'.", listOf(ConstantToken(4), SymbolToken("+"), CloseBracketToken, QuoteToken, DotToken)),
+        )
+
+        @JvmStatic
+        fun `negative numbers`() = listOf(
+            arguments("-2", listOf(ConstantToken(-2))),
+            arguments("-2345", listOf(ConstantToken(-2345))),
+            arguments("-53 - -123", listOf(ConstantToken(-53), SymbolToken("-"), ConstantToken(-123))),
+            arguments("-2 - 2", listOf(ConstantToken(-2), SymbolToken("-"), ConstantToken(2))),
+        )
+
+        @JvmStatic
+        fun spaces() = listOf(
+            arguments("      ", listOf<Token>()),
+            arguments("  4 +  ", listOf(ConstantToken(4), SymbolToken("+"))),
+            arguments(" aba  foo caba   ", listOf(SymbolToken("aba"), SymbolToken("foo"), SymbolToken("caba"))),
+        )
+
+        @JvmStatic
+        fun unary() = listOf(
+            arguments("+4", listOf(ConstantToken(4))),
+            arguments("+67", listOf(ConstantToken(67))),
+            arguments(
+                "+1 -  -2 ++3",
+                listOf(ConstantToken(1), SymbolToken("-"), ConstantToken(-2), SymbolToken("+"), ConstantToken(3))
+            ),
+        )
+
+        @JvmStatic
+        fun symbols() = listOf(
+            arguments("foo bar zog-zog?", listOf(SymbolToken("foo"), SymbolToken("bar"), SymbolToken("zog-zog?"))),
+            arguments("baz-15 foo+15", listOf(SymbolToken("baz-15"), SymbolToken("foo"), ConstantToken(15))),
+            arguments(
+                "<=> *42. #hash-tag' 'hi!.##", listOf(
+                    SymbolToken("<=>"), SymbolToken("*42"), DotToken,
+                    SymbolToken("#hash-tag"), QuoteToken, QuoteToken, SymbolToken("hi!"), DotToken, SymbolToken("##")
+                )
+            ),
+        )
+
+        @JvmStatic
+        fun brackets() = listOf(
+            arguments("( ()", listOf(OpenBracketToken, OpenBracketToken, CloseBracketToken)),
+            arguments(
+                "(-2 3 aba)",
+                listOf(OpenBracketToken, ConstantToken(-2), ConstantToken(3), SymbolToken("aba"), CloseBracketToken)
+            ),
+            arguments(
+                ")aba(caba)",
+                listOf(CloseBracketToken, SymbolToken("aba"), OpenBracketToken, SymbolToken("caba"), CloseBracketToken)
+            ),
+            arguments(
+                "(.-25##-15)'-8", listOf(
+                    OpenBracketToken,
+                    DotToken,
+                    ConstantToken(-25),
+                    SymbolToken("##-15"),
+                    CloseBracketToken,
+                    QuoteToken,
+                    ConstantToken(-8)
+                )
+            ),
+        )
     }
 }
